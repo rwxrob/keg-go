7bff03209e525db70d68eee32b8c584fc463236d
Rob Muhlestein <rob@rwx.gg>
2022-12-18 03:21:04 -0500

    Add more scaffolding

--- libkeg.go
+++ libkeg.go
@@ -1,1 +1,24 @@
 package libkeg
+
+// Index interface assumes any source of relational data store
+type Index interface {
+}
+
+type index struct {
+	Path string
+}
+
+// CreateIndex creates a Index implementation that is backed by
+// a sqlite3 database file at location specified by path. If it does not
+// exist and returns a reference to it. Otherwise, just opens and
+// returns a new reference to it.
+func CreateIndex(path string) (*index, error) {
+
+	index := new(index)
+
+	// detect existing db and just open if so
+
+	// create a new sqlite3 db
+
+	return index, nil
+}
---
+++ utils.go
@@ -0,0 +1,13 @@
+package libkeg
+
+import "os"
+
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func notExists(path string) bool {
+	_, err := os.Stat(path)
+	return os.IsNotExist(err)
+}
