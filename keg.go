5dc6e508c710b33b6299ad44bb231c5dfeb1e059
Rob Muhlestein <rob@rwx.gg>
2022-12-20 02:23:54 -0500

    Add FetchIndex

--- keg.go
+++ keg.go
@@ -1,6 +1,10 @@
 package keg
 
 import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -12,4 +16,50 @@ func exists(path string) bool {
 func notExists(path string) bool {
 	_, err := os.Stat(path)
 	return os.IsNotExist(err)
+}
+
+// stringify first looks for string, []byte, []rune, and io.Reader types
+// and if matched returns a string with their content and the string
+// type.
+//
+// String converts whatever remains to that types fmt.Sprintf("%v")
+// string version (but avoids calling it if possible). Be sure you use
+// things with consistent string representations. Keep in mind that this
+// is extremely high level for rapid tooling and prototyping.
+func stringify(in any) string {
+	switch v := in.(type) {
+	case string:
+		return v
+	case []byte:
+		return string(v)
+	case []rune:
+		return string(v)
+	case io.Reader:
+		buf, err := io.ReadAll(v)
+		if err != nil {
+			log.Println(err)
+		}
+		return string(buf)
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
+
+func fetch(url string) ([]byte, error) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return body, err
+	}
+
+	if resp.StatusCode != 200 {
+		return body, ErrFetch{resp}
+	}
+
+	return body, nil
 }
--- keg_test.go
+++ keg_test.go
@@ -1,6 +1,9 @@
 package keg
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -14,4 +17,20 @@ func TestNotExists(t *testing.T) {
 	if !notExists("testdata/nope") {
 		t.Error(`notExists fails to confirm missing file`)
 	}
+}
+
+func TestFetch(t *testing.T) {
+
+	handler := http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "2\t2022-12-19 11:40:01Z\tSome title\t0,2\n")
+		})
+	svr := httptest.NewServer(handler)
+	defer svr.Close()
+
+	buf, err := fetch(svr.URL)
+	if err != nil || string(buf) != "2\t2022-12-19 11:40:01Z\tSome title\t0,2\n" {
+		t.Error(`fetch failed`, err)
+	}
+
 }
